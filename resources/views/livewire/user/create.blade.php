e7d587ef6bb55e15b580e6f6267d270cd6e9b5db
LaercioLopesLL <laerciolopesll@gmail.com>
2020-10-23 16:34:32 -0300

    npm run dev

---
+++ resources/views/livewire/user/create.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create User') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl rounded mx-auto sm:px-6 lg:px-8 text-sm font-mono">
+            <label class="">Nome</label>
+            <input class="my-input" type="text"> <button class="my-btn btn-blue">Save</button>
+        </div>
+    </div>
+</div>
