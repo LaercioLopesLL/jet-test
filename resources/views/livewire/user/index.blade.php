9e5248eada6a97055f76836cd2c5ff819c42c92a
LaercioLopesLL <laerciolopesll@gmail.com>
2021-01-04 22:36:36 -0300

    Usando componente de input do jetstream

--- resources/views/livewire/user/index.blade.php
+++ resources/views/livewire/user/index.blade.php
@@ -3,7 +3,7 @@
     <div>
         @if (session()->has('message'))
         <div x-data="{ open: true }" x-show.transition.out.duration.500ms="open"
-            class="flex justify-between align-middle bg-green-300 bg-opacity-50 text-green-900 rounded mb-2">
+            class="flex justify-between align-middle bg-blue-300 bg-opacity-50 text-blue-900 rounded mb-2">
             <span class="py-3 pl-3">{{ session('message') }}</span>
             <button class="focus:outline-none p-3" @click="open = false">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="/currentColor">
@@ -23,21 +23,14 @@
                 <div class="mt-2">
                     <div class="grid  md:grid-cols-2 gap-4">
                         <div>
-                            <label class="block text-gray-500" for="">test</label>
-                            <x-jet-input name="test" class="w-full"></x-jet-input>
+                            <label class="block text-gray-500" for="name">Nome</label>
+                            <x-jet-input name="name" class="w-full" wire:model.defer="name" />
+                            <x-jet-input-error for="name" />
                         </div>
                         <div>
-                            <label class="block text-gray-500" for="">Nome</label>
-                            <input @if($errors->has('name')) class="my-input w-full border-red-300 focus:border-red-500"
-                            @else class="my-input w-full" @endif type="text" wire:model.defer="name">
-                            @error('name') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
-                        </div>
-                        <div>
-                            <label class="block text-gray-500" for="">Email</label>
-                            <input @if($errors->has('email')) class="my-input w-full border-red-300
-                            focus:border-red-500" @else class="my-input w-full" @endif type="email"
-                            wire:model.defer="email">
-                            @error('email') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
+                            <label class="block text-gray-500" for="email">Email</label>
+                            <x-jet-input type="email" name="email" class="w-full" wire:model.defer="email" />
+                            <x-jet-input-error for="email" />
                         </div>
                     </div>
                 </div>
@@ -61,9 +54,9 @@
         </div>
     </div>
     <div class="w-full mt-4 text-gray-700">
-        <div class="flex place-content-between">
-            <div class="text-sm text-gray-600">
-                <select id="pagination" class="rounded p-2 focus:outline-none border border-gray-400 border-opacity-50"
+        <div class="flex flex-col-reverse md:flex-row md:place-content-between">
+            <div class="text-sm text-gray-600 mb-2" class="block md:inline">
+                <select id="pagination" class="rounded p-3 focus:outline-none border border-gray-400 border-opacity-50"
                     wire:model="recordsPerPage">
                     <option>5</option>
                     <option>10</option>
@@ -71,9 +64,7 @@
                 </select>
                 registros por página
             </div>
-            <input style="width: 300px; max-width: 300px;"
-                class="rounded py-1 px-2 focus:outline-none border border-gray-400 border-opacity-50 focus:border-blue-500"
-                placeholder="Buscar" name="search" type="search" wire:model="search">
+            <x-jet-input class="block w-full md:w-1/2 md:inline mb-2" name="search" placeholder="Pesquisa" wire:model="search" />
         </div>
         <div class="shadow-md rounded mt-1 border-t border-gray-300 border-opacity-50">
             <table class="text-left w-full border-collapse">
